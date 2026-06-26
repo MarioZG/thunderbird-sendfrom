474723702d685e01756df01c2d644e1cdbfaf049
MarioZG <10474989+MarioZG@users.noreply.github.com.>
2026-06-26 18:30:53 +0100

    inial commit

---
+++ src/background.js
@@ -0,0 +1,24 @@
+// background.js
+browser.compose.onComposeStateChanged.addListener(async (tab, state) => {
+  // if (state.type === "reply") {
+  //   console.log("Reply compose opened:", tab.id);
+  //}
+
+  console.debug("Reply to addon: start:");
+
+  const details = await browser.compose.getComposeDetails(tab.id);
+  console.debug("Reply to addon: details validation");
+  if (!details) return;
+
+  const originalMessage = await browser.messages.get(details.relatedMessageId);
+  console.debug(`Reply to addon: originalMessage validation ${originalMessage}`);
+  if (!originalMessage.recipients || originalMessage.recipients.length != 1) return;
+
+  const originaTo = originalMessage.recipients[0]
+  console.debug(`Reply to addon: originalTo:${originaTo} validation`);
+
+  browser.compose.setComposeDetails(tab.id, { from: originaTo });
+  console.log("Update from address:", identities);
+
+}
+);
